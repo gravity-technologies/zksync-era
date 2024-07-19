9d053e3ed169ea7a1a5db9d4e0307917a4e4cbdb
Haoze <haoze@grvt.io>
2024-07-19 17:33:01 +0800

    Allow skipping chain governance init

--- infrastructure/zk/src/contract.ts
+++ infrastructure/zk/src/contract.ts
@@ -99,6 +99,8 @@ export async function deployL2ThroughL1({
     await utils.spawn(
         `yarn l2-contracts deploy-shared-bridge-on-l2-through-l1 ${args.join(' ')} ${
             localLegacyBridgeTesting ? '--local-legacy-bridge-testing' : ''
+        } ${
+            !!process.env.SKIP_INITIALIZE_CHAIN_GOVERNANCE ? '--skip-initialize-chain-governance true' : ''
         } | tee deployL2.log`
     );
 
